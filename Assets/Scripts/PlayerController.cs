decd9a182366ab7b249c8d42b39292e08d8c2223
Evenilink <evenilink.code@gmail.com>
2018-10-17 05:07:57 +0100

    Trying to make the player collider work with the level.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,39 @@
 
 public class PlayerController : MonoBehaviour {
 
+    [Header("Movement Settings")]
     [SerializeField] private float movementSpeed = 1f;
+
+    [Header("Raycast")]
+    [SerializeField] private Transform leftRaycast;
+    [SerializeField] private Transform rightRaycast;
+
+    private enum CubeSide {
+        FRONT,
+        BACK,
+        LEFT,
+        RIGHT
+    }
+    private CubeSide currSide = CubeSide.FRONT;
 
     private void Start() {
 
     }
 
     protected void Update() {
+        if (!GetComponent<MeshRenderer>().enabled)
+            return;
+
+        Debug.DrawRay(transform.position, -transform.up * 10f, Color.red);
+        if (!Physics.Raycast(leftRaycast.position, transform.forward, 10f)) {
+            switch(currSide) {
+                case CubeSide.FRONT:
+                    currSide = CubeSide.RIGHT;
+                    transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z - 90f);
+                    break;
+            }
+        }
+
         HandleMovement();
     }
 
