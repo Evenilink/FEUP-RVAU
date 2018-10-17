29c298064125cae332ee84607975dc72246047b9
Evenilink <evenilink.code@gmail.com>
2018-10-17 04:13:30 +0100

    Added basic player controller. Canmove left and right.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private float movementSpeed = 1f;
+
+    private void Start() {
+
+    }
+
+    protected void Update() {
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
+        float hInput = Input.GetAxis("Horizontal");
+        Vector3 horizontalMovement = transform.right * hInput;
+        transform.position += horizontalMovement * movementSpeed * Time.deltaTime;
+    }
 }
