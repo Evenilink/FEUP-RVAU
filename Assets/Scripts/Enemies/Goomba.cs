a4bb19b45b8a1a9461fd5ee8cc037eee5ff88215
Evenilink <evenilink.code@gmail.com>
2018-10-20 18:59:14 +0100

    Started working on the enemy.

---
+++ Assets/Scripts/Enemies/Goomba.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Goomba : MonoBehaviour {
+
+    [SerializeField] private float movSpeed;
+    [SerializeField] private bool startFacingRight = false;
+    [SerializeField] private LayerMask levelMask;
+
+    private void Start() {
+        if (!startFacingRight)
+            transform.rotation = Quaternion.Euler(0, 180, 0) * transform.rotation;
+    }
+
+    void Update () {
+        transform.position += transform.right * movSpeed * Time.deltaTime;
+        if (Physics.Raycast(transform.position, transform.right, 0.09f, levelMask))
+            Flip();
+	}
+
+    private void Flip() {
+        transform.rotation = Quaternion.Euler(0, 180, 0) * transform.rotation;
+    }
+}
