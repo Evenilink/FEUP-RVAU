7dfd4768b0830c644e7594f8778e9d9cc36ce378
Evenilink <evenilink.code@gmail.com>
2018-10-21 18:30:52 +0100

    Implementing shooting enemy.

---
+++ Assets/Scripts/Components/PlayerMovementComponent.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerMovementComponent : MovementComponent {
+
+    private float lastHeight;
+    private PlayerController pc;
+
+    private void Awake() {
+        base.Awake();
+        pc = GetComponent<PlayerController>();
+        lastHeight = transform.position.y;
+    }
+
+    private void FixedUpdate() {
+        base.FixedUpdate();
+    }
+
+    public void Move(float hInput) {
+        if (hInput == 0)
+            return;
+        if (hInput < 0 && isRight || hInput > 0 && !isRight)
+            RotatePlayer(ROTATE_FLIP);
+
+        if (hInput != 0) {
+            bool obstacleDetected, canKeepMoving;
+            analyser.CheckObstacle(out obstacleDetected, out canKeepMoving);
+            if (canKeepMoving) {
+                // No need to calculate if there's an abism if we know there's an obstacle in front of the player.
+                if (!obstacleDetected && analyser.CheckAbism())
+                    RotatePlayer(isRight ? ROTATE_FROM_RIGHT : -ROTATE_FROM_RIGHT);
+                // Since we're rotating the character when he flips (instead of applying a negative scale) we're only interested in the absolute value of the input.
+                Vector3 horizontalMovement = transform.right * Mathf.Abs(hInput);
+                transform.position += horizontalMovement * movementSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+    protected override void OnReachedGround() {
+        if (transform.position.y > lastHeight) {
+            LevelScrollingManager.Instance().ScrollToHeight(transform.position.y - lastHeight);
+            lastHeight = transform.position.y;
+            pc.SetCheckpointInfo(pc.GetCheckpointInfo().position - new Vector3(0, transform.position.y - lastHeight, 0), pc.GetCheckpointInfo().rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/ShooterShell.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ShooterShell : BaseEnemy {
+
+    [Header("Shooting")]
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletInsPosition;
+    // Time it takes to fire a bullet.
+    [SerializeField] private float fireRate = 1.5f;
+    private float shootTime = 0f;
+
+    [Header("Jump")]
+    [SerializeField] private bool canJump = true;
+    [SerializeField] private float jumpRate = 3f;
+    [SerializeField] private float jumpProbability = 0.6f;
+
+    [Header("Components")]
+    private MovementComponent movComp;
+
+    private void Awake() {
+        base.Awake();
+        movComp = GetComponent<MovementComponent>();
+    }
+
+    private void Update() {
+        shootTime -= Time.deltaTime;
+        if (shootTime <= 0)
+            Fire();
+    }
+
+    private void Fire() {
+        Instantiate(bullet, bulletInsPosition.position, transform.rotation, transform.parent);
+        shootTime = fireRate;
+    }
+}
