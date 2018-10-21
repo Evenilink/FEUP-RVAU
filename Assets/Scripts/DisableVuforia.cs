491917959f44e06456724bf925427840b8c404df
Fr0sk <the.fr0sk@gmail.com>
2018-10-21 15:08:35 +0100

    Added a script to test without vuforia

---
+++ Assets/Scripts/DisableVuforia.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableVuforia : MonoBehaviour {
+    [SerializeField] bool disableVuforia = false;
+    [SerializeField] GameObject levelRoot;
+    [SerializeField] float mouseSensitivity = 10;
+    private void Awake() {
+        if (disableVuforia) {
+            GetComponent<CustomTrackableEventHandler>().enabled = false;
+            transform.Find("ChildTargets").gameObject.SetActive(false);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (disableVuforia) {
+            float v = mouseSensitivity * Input.GetAxis("Mouse X");
+            levelRoot.transform.Rotate(0, 0, v);
+
+        }
+	}
+}
