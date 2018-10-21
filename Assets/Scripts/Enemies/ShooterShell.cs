847ce4c164662b31f8f15c758fb67ee55d906853
Evenilink <evenilink.code@gmail.com>
2018-10-22 00:39:50 +0100

    Implemented full Shooter Shell (shoot based enemy).

--- Assets/Scripts/Enemies/ShooterShell.cs
+++ Assets/Scripts/Enemies/ShooterShell.cs
@@ -4,32 +4,45 @@ public class ShooterShell : BaseEnemy {
 
     [Header("Shooting")]
     [SerializeField] private GameObject bullet;
-    [SerializeField] private Transform bulletInsPosition;
+    [SerializeField] private Transform bulletSpawnPoint;
     // Time it takes to fire a bullet.
     [SerializeField] private float fireRate = 1.5f;
     private float shootTime = 0f;
 
     [Header("Jump")]
     [SerializeField] private bool canJump = true;
-    [SerializeField] private float jumpRate = 3f;
-    [SerializeField] private float jumpProbability = 0.6f;
+    [SerializeField] private float minJumpRate = 1f;
+    [SerializeField] private float maxJumpRate = 3f;
+    private float jumpTime;
 
     [Header("Components")]
     private MovementComponent movComp;
 
     private void Awake() {
+        Random.InitState((int)System.DateTime.Now.Ticks);
         base.Awake();
         movComp = GetComponent<MovementComponent>();
+        shootTime = fireRate;
+        jumpTime = Random.Range(minJumpRate, maxJumpRate);
     }
 
     private void Update() {
         shootTime -= Time.deltaTime;
         if (shootTime <= 0)
             Fire();
+
+        if (canJump) {
+            jumpTime -= Time.deltaTime;
+            if (jumpTime <= 0) {
+                if (movComp.IsGrounded())
+                    movComp.Jump();
+                jumpTime = Random.Range(minJumpRate, maxJumpRate);
+            }
+        }
     }
 
     private void Fire() {
-        Instantiate(bullet, bulletInsPosition.position, transform.rotation, transform.parent);
+        Instantiate(bullet, bulletSpawnPoint.position, transform.rotation, transform.parent);
         shootTime = fireRate;
     }
 }
