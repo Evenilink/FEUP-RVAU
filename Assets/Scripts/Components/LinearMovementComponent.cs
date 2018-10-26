884f788797db63c2cdd9fc8506928174bc635e35
Evenilink <evenilink.code@gmail.com>
2018-10-26 01:54:41 +0100

    Implemented Cloud System and weather forecast is ready to be read!

---
+++ Assets/Scripts/Components/LinearMovementComponent.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class LinearMovementComponent : MonoBehaviour {
+
+    [SerializeField] private float aliveTime = 0f;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private Axis axisToMove;
+    private enum Axis {
+        FORWARD,
+        BACK,
+        RIGHT,
+        LEFT,
+        UP,
+        DOWN
+    }
+    private Vector3 selectedAxis;
+
+    private void Awake() {
+        if (aliveTime > 0)
+            Destroy(gameObject, aliveTime);
+        switch (axisToMove) {
+            case Axis.FORWARD:
+                selectedAxis = transform.forward;
+                break;
+            case Axis.BACK:
+                selectedAxis = -transform.forward;
+                break;
+            case Axis.RIGHT:
+                selectedAxis = transform.right;
+                break;
+            case Axis.LEFT:
+                selectedAxis = -transform.right;
+                break;
+            case Axis.UP:
+                selectedAxis = transform.up;
+                break;
+            case Axis.DOWN:
+                selectedAxis = -transform.up;
+                break;
+        }
+    }
+	
+	private void Update () {
+        transform.position += selectedAxis * moveSpeed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Weather/CloudSystem.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class CloudSystem : MonoBehaviour {
+
+    [Header("System Settings")]
+    [SerializeField] private GameObject cloudPrefab;
+    [SerializeField] private Mesh[] cloudMeshes;
+    [SerializeField] private int numClouds = 10;
+
+    [Header("Position Spawn Info")]
+    [SerializeField] private float minSpawnX;
+    [SerializeField] private float maxSpawnX;
+    [SerializeField] private float minSpawnY;
+    [SerializeField] private float maxSpawnY;
+    [SerializeField] private float minSpawnZ;
+    [SerializeField] private float maxSpawnZ;
+
+    private GameObject[] clones;
+
+	private void Awake () {
+        Debug.Log(new Vector3((Mathf.Abs(maxSpawnX) + Mathf.Abs(minSpawnX)) / 2, (Mathf.Abs(maxSpawnY) + Mathf.Abs(minSpawnY)) / 2, (Mathf.Abs(maxSpawnZ) + Mathf.Abs(minSpawnZ)) / 2));
+        clones = new GameObject[numClouds];
+		for (int i = 0; i < numClouds; i++) {
+            Vector3 spawnPosition = new Vector3(Random.Range(minSpawnX, maxSpawnX), Random.Range(minSpawnY, maxSpawnY), Random.Range(minSpawnZ, maxSpawnZ));
+            clones[i] = Instantiate(cloudPrefab, spawnPosition, transform.rotation, transform);
+            clones[i].GetComponent<MeshFilter>().mesh = cloudMeshes[Random.Range(0, cloudMeshes.Length - 1)];
+        }
+	}
+
+    private void Update() {
+        for (int i = 0; i < numClouds; i++) {
+            if (clones[i].transform.position.z >= maxSpawnZ)
+                RandomizeCloud(clones[i]);
+        }
+    }
+
+    private void RandomizeCloud(GameObject cloud) {
+        cloud.transform.position = new Vector3(Random.Range(minSpawnX, maxSpawnX), Random.Range(minSpawnY, maxSpawnY), minSpawnZ);
+        cloud.GetComponent<MeshFilter>().mesh = cloudMeshes[Random.Range(0, cloudMeshes.Length - 1)];
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.grey;
+        Vector3 center = new Vector3((maxSpawnX + minSpawnX) / 2, (maxSpawnY + minSpawnY) / 2, (maxSpawnZ + minSpawnZ) / 2);
+        Vector3 size = new Vector3(Mathf.Abs(maxSpawnX) + Mathf.Abs(minSpawnX), Mathf.Abs(maxSpawnY) + Mathf.Abs(minSpawnY), Mathf.Abs(maxSpawnZ) + Mathf.Abs(minSpawnZ));
+        Gizmos.DrawWireCube(center, size);
+    }
+}
